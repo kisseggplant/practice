68adf049aa98d0ef800558e581a5df83ed9f10fc
kisseggplant <119676838+kisseggplant@users.noreply.github.com>
2024-09-17 14:33:47 +0800

    Update 格式化输入输出.java

--- java/格式化输入输出.java
+++ java/格式化输入输出.java
@@ -16,25 +16,25 @@ public class Main{
                 }
     
                 else if(choice.equals("int")){
-                        String line = scanner.nextLine();
-                        String [] parts = line.split("\\s+");
-                        int [] arrd = new int[3];
-                        for (int i = 0; i < 3; i++){
-                            arrd[i] = Integer.parseInt(parts[i].trim());
-                        }
-                        System.out.println("choice="+choice);
-                        System.out.printf("%d\n",arrd[0]+arrd[1]+arrd[2]);
+                    String line = scanner.nextLine();
+                    String [] parts = line.split("\\s+");
+                    int [] arrd = new int[3];
+                    for (int i = 0; i < 3; i++){
+                        arrd[i] = Integer.parseInt(parts[i].trim());
+                    }
+                    System.out.println("choice="+choice);
+                    System.out.printf("%d\n",arrd[0]+arrd[1]+arrd[2]);
                 }
                 else if(choice.equals("str")){
-                        String line = scanner.nextLine();
-                        String [] parts = line.split("\\s+");
-                        System.out.println("choice="+choice);
-                        System.out.printf("%s%s%s\n",parts[2],parts[1],parts[0]);
+                    String line = scanner.nextLine();
+                    String [] parts = line.split("\\s+");
+                    System.out.println("choice="+choice);
+                    System.out.printf("%s%s%s\n",parts[2],parts[1],parts[0]);
                 }
                 else if(choice.equals("line")){
-                        String line = scanner.nextLine();
-                        System.out.println("choice="+choice);
-                        System.out.printf("%s\n",line.toUpperCase());
+                    String line = scanner.nextLine();
+                    System.out.println("choice="+choice);
+                    System.out.printf("%s\n",line.toUpperCase());
                 }
                 else{
                     System.out.println("choice="+choice);
